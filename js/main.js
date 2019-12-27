f6d53275e28a682729257ad4b33eda411d8db9e8
csslick <digitone@naver.com>
2019-12-27 16:18:29 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,4 +6,40 @@ $(function(){
     $('.nav_group ').toggleClass('show');
   });
 
-});
+  // SNS bx slider
+  var width = $(window).width();  
+  (width < 800) ? _minSlides = 2 : _minSlides = 4;
+  (width < 800) ? _maxSlides = 2 : _maxSlides = 4;
+  console.log(width);
+
+  var settings1 = {
+    minSlides: 4,
+    maxSlides: 4,
+    moveSlides: 1,
+    slideWidth: 1920,
+  };
+  var settings2 = {
+    minSlides: 2,
+    maxSlides: 2,
+    moveSlides: 1,
+    slideWidth: 1920,
+  };
+
+  var slider = $('#sns .slider').bxSlider({
+    minSlides: _minSlides,
+    maxSlides: _maxSlides,
+    moveSlides: 1,
+    slideWidth: 1200,
+    onSliderResize: function(){
+      width = $(window).width();
+      console.log(width);
+      if(width < 800){
+        slider.reloadSlider(settings2);
+      } else {
+        slider.reloadSlider(settings1);
+      }
+    }
+  })
+
+
+}); // $end jquery
