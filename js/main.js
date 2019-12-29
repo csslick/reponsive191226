40a5a9ce43dde14757363c3331d58a7713a00961
csslick <digitone@naver.com>
2019-12-29 19:47:32 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -27,10 +27,14 @@ $(function(){
   var width = window.innerWidth;
   var slider;
   
-  // 화면 크기가 768 보다 크면 setting1 아니면 setting2
-    (width > 767) ? 
+  // 이미지 로드 채크
+    imagesLoaded( document.querySelector('.slider'), function() {
+      console.log('all images are loaded');
+       // 화면 크기가 768 보다 크면 setting1 아니면 setting2
+      (width > 767) ? 
       slider = $('#sns .slider').bxSlider(settings1) :
       slider = $('#sns .slider').bxSlider(settings2)
+    });
 
   // 창크기 변화시 슬라이더 설정 변경
   $(window).resize(function(){
